876f3f8424bd20df3f352f9e8ab4c9a06c7bc553
fulcrum066 <dragonkyle@icloud.com>
2025-03-09 23:30:14 +1100

    Modifications made to Basic SSH Honeypot Project

--- HoneyPot/server_V3.py
+++ HoneyPot/server_V3.py
@@ -1,5 +1,5 @@
 #ssh localhost -p 2292
-#ssh -p 2292 kyle@192.168.98.99 
+#ssh -p 22 kyle@0.0.0.0
 
 import socket 
 import threading 
@@ -12,11 +12,13 @@ import os
 #Remember to uninstall nacl 
 #Remember to uninstall pynacl
 
+count = 0
 class SSHServer(paramiko.ServerInterface):
 
     def check_auth_password(self, username, password):
         #Change the number of allowed attempts for a real scenario
         while True:
+            global file
             file.write(str(password) + "\n")
             print(password)
             if password == "HelloWorld":
@@ -34,65 +36,70 @@ class SSHServer(paramiko.ServerInterface):
         return True 
 
     def check_channel_exec_request(self, channel, command):
-        command_list = {
-            "ls": "file1.txt file2.log \n",
-            "whoami": "root\n",
-            "pwd": "/home/user\n",
-        }
-        response = command_list.get(command.strip())
-
-        if response != None:
-            channel.send(response)
-            return True
-
-        else:   
-            channel.send("Start inputting commands:\n")
-            return True
+        return True
     
-def inputCommands():
-    channel.send("Welcome to SSH Server\n")
-    while True:
-        #print("called 1")
-        command = channel.recv(1024).decode().strip()
-        file.write(str(command) + "\n")
-        print(command)
-        if command != "exit":
-            SSHServer.check_channel_exec_request(SSHServer, channel=channel, command=command)
-        else: 
-            channel.close()
-            file.close()
-            break
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return False
 
-def log():
-    count = 0
-    file = open("log" + str(count) + ".txt", "w")
-                
+def inputCommands(): 
+    global file
+    command_list = {
+        "ls": "file1.txt file2.log \n",
+        "whoami": "root\n",
+        "pwd": "/home/user\n",
+    }
+        
+    channel.send("Start Inputting Your Commands:\r\n")
+    while True:
+        command = channel.recv(1024)
+        if command != None:
+            command = command.decode('utf-8')
+            if command != "exit\n":
+                response = command_list.get(command.strip())
+                if response != None:
+                    print(command.strip())
+                    file.write(command)
+                    channel.send(response)
+                    
+                else:
+                    channel.send("Invalid Command\n")
+                    print(command.strip())
+                    file.write(command)
+
+            else:
+                print(command.strip())
+                file.write(command)
+                file.close()
+                break
+            
+
+
 if __name__ == "__main__":
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-    server.bind(('0.0.0.0', 2292))
+    server.bind(('0.0.0.0', 22))
     server.listen()
     print("Listening...")
 
     while True:
-        file = open("log.txt", "w")
-
-        logging = threading.Thread(target=log, args=())
-        logging.start()
-        
         clientSocket, clientAddr = server.accept()
+        
+        print(count)
+        count = count + 1
+        file = open("log" + str(count) + ".txt", "w")
 
         file.write("New Connection From {addr}\n".format(addr = clientAddr))
         print("New Connection From {addr}".format(addr = clientAddr))
+
         transport = paramiko.Transport(clientSocket)
-    
-        key = paramiko.RSAKey.from_private_key_file('Honeypot')
+        key = paramiko.RSAKey.from_private_key_file('/Users/kyle/Documents/GitHub/kyle-ritchie/HoneyPot/Honeypot')
         transport.add_server_key(key) 
         myServer = SSHServer()
-
         transport.start_server(server=myServer)
+        
         channel = transport.accept(20)
+        channel.send("Welcome to SSH Server\r\n")
 
-        #Fix issue with relogging in here
-        inputCommands = threading.Thread(target=inputCommands, args=())
-        inputCommands.start()
+        inputCommands()
+        channel.close()
